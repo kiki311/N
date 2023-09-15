9e40f4ff7e1e03b18dcb439d8f48c5124a0f571f
kiki311 <sun.xiaoju@northeastern.edu>
2023-09-15 07:28:41 +0200

    Added About Me button

--- app/src/main/java/edu/northeastern/numad23fa_amyku/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad23fa_amyku/MainActivity.java
@@ -3,12 +3,31 @@ package edu.northeastern.numad23fa_amyku;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnToast=findViewById(R.id.btnToast);
+
+        btnToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Amy sunxiaoju@northeastern.edu", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+
     }
 }
